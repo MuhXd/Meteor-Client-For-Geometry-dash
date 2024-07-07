f7ea7f9dfad5dbe360ea2e606c8efb61d4cc884a
Viper <momo2010sofia@gmail.com>
2024-07-07 18:07:47 +0100

    :(

--- src/Meteor/Mods/Tabs/ExampleTab.cpp
+++ src/Meteor/Mods/Tabs/ExampleTab.cpp
@@ -1,4 +1,9 @@
-#include <Meteor/UiMain/modhandler.hpp>
 #include <Meteor/UiMain/theme.hpp>  
+#include <Meteor/MeteorClass.hpp> 
+#include <Meteor/UiMain/modhandler.hpp>
 
+Meteor::Types::MeteorTab Type = {
+    "HELLO","ID",1
+};
 
+Meteor::UI::Theme::NewTab(Type);
